8e3865d07abedaa5c784fa9bd6503e32fc046092
Andi Bezhani <abezhani@my.wctc.edu>
2018-09-10 21:21:17 -0500

    Week 3

--- Week3Assignment/out/artifacts/Reasonable_war_exploded/index.jsp
+++ Week3Assignment/out/artifacts/Reasonable_war_exploded/index.jsp
@@ -1,14 +1,23 @@
 <html>
 <body>
-<h1>Simple Servlet</h1>
-<a href="simple?message=This is a message">Simple Site</a>
+<h1>Simple Student Search</h1>
+<a href="simple?message=This is a message">Simple Student Search</a>
 
 
-<form action = "address" method = "GET">
-    First Name: <input type = "text" name = "fname">
-    <br />
-    Last Name: <input type = "text" name = "lname" />
+<form action = "simple" method = "GET">
+
+    Search: <input type = "text" name = "searchtext" />
     <input type = "submit" value = "Submit" />
+    <fieldset>
+        <legend>Please select the parameter you would like to search by:</legend>
+        <input type = "radio" name = "parameter" value="Account Number"/>Account Number<br>
+        <input type = "radio" name = "parameter"  value="Last Name"/>Last Name<br>
+        <input type = "radio" name = "parameter" value="First Name"/>First Name<br>
+        <input type = "radio" name = "parameter" value="E-Mail"/>E-Mail<br>
+        <input type = "radio" name = "parameter" value="iPadDr"/>iPadDr<br>
+    </fieldset>
+
+    <input type = "submit" value = "Submit Answers" />
 </form>
 
 </body>
--- Week3Assignment/src/simple/SimpleServlet.java
+++ Week3Assignment/src/simple/SimpleServlet.java
@@ -12,11 +12,6 @@ import java.util.ArrayList;
 @WebServlet(name = "SimpleServlet")
 public class SimpleServlet extends HttpServlet
 {
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
-    {
-
-    }
-
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
     {
         // Set response content type
@@ -27,21 +22,57 @@ public class SimpleServlet extends HttpServlet
         out.println("<html><head></head><body><h1>Simple Web Page</h1><p>The Test.");
         out.println(request.getParameter("message"));
 
-        //This text was inserted (copy/pasted) from the main class of the student program
+        //This text was inserted (copy/pasted) from the main class of the student program 9/5=18
         Students theStudents = new Students();
 
         ArrayList<Individual> result;
+        String searchvalue = request.getParameter("searchtext");
+        String findvalue = request.getParameter("parameter");
+        FindColumns cols = null;
+        switch(findvalue)
+            {
+                case "Account Number": cols = FindColumns.ACCTNUM;
+                break;
 
-        result = theStudents.findStudents(FindColumns.ACCTNUM, "25");
-        out.println("<table border='1'><tr><th>Account Number</th><th>First Name</th><th>Last Name</th>");
+                case "Last Name": cols = FindColumns.LNAME;
+                break;
+
+                case "First Name": cols = FindColumns.FNAME;
+                break;
+
+                case "E-Mail": cols = FindColumns.EMAIL;
+                break;
+
+                case "iPadDr": cols = FindColumns.IPADDR;
+                break;
+            }
+
+        result = theStudents.findStudents(cols, searchvalue);
+        out.println("<table border='1'><tr><th>Account Number</th><th>First Name</th><th>Last Name</th><th>E-Mail</th><th>iPadDr</th>");
         for (Individual i: result)
         {
             out.println("<tr>"+"<td>"+ i.getAccountNumber() + "</td>" + " " + "<td>" + i.getFname() + "</td>" + " " + "<td>" +
-                    i.getLname() + "</td>"+"</tr>");
+                    i.getLname() + "</td>"+ "<td>"+ i.getEmail() + "</td>" +"<td>"+ i.getIpAddress() + "</td>" + "</tr>");
         }
-        //This text was inserted (copy/pasted) from the main class of the student program
+        //This text was inserted (copy/pasted) from the main class of the student program 9/5/18
         out.println("</table>");
         out.println("</p></body></html>");
+
+        /*out.println("</tr>\n</table>\n");
+        String fullName = request.getParameter("fname");
+        String comicType = request.getParameter("lname");
+
+        out.println("<hr><h3>Using Parameter Names</h3><ul>\n" +
+                "  <li><b>full_name</b>: "
+                + fullName + "</li>\n" + "  <li><b>comic_type</b>: "
+                + comicType + "</li>\n" + "  <li><b>parameter</b>: "
+                + company + "</li>\n" + "</ul>\n");
+        out.println("</body></html>");*/
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+    {
+        doGet(request,response);
     }
 
 
